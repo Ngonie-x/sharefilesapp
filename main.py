185ffc8d78a4b7cb146de5c4bba5c83ff076f36a
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2022-11-17 15:47:43 +0200

    PC setup

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import shelve
+import server
+import client
+import socket
+from threading import Thread
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.dialog import MDDialog
@@ -8,35 +13,32 @@ from kivymd.uix.filemanager import MDFileManager
 import os
 from kivy.utils import platform
 
-## Android settings
-from android.storage import primary_external_storage_path
-primary_ext_storage = primary_external_storage_path()
 
 if platform == 'android':
     import android
+    # Android settings
+    from android.storage import primary_external_storage_path
+    primary_ext_storage = primary_external_storage_path()
     from android.permissions import request_permissions, Permission
-    request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE, Permission.ACCESS_NETWORK_STATE, Permission.INTERNET])
+    request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE,
+                        Permission.ACCESS_NETWORK_STATE, Permission.INTERNET])
+
+else:
+    primary_ext_storage = os.getcwd()
+
 
 app_path = os.path.dirname(os.path.abspath(__file__))
 
-## Networking
-import socket, os, shelve
-import client, server
-from threading import Thread
+# Networking
 
 
 # shelve files
-
 
 
 #--------------------Custom Elements--------------------------#
 
 class DialogContent(MDBoxLayout):
     pass
-
-
-
-
 
 
 #------------------------Main App class -----------------------#
@@ -51,7 +53,6 @@ class MainApp(MDApp):
             exit_manager=self.exit_manager,
             select_path=self.select_path,
         )
-
 
         self.dialog = None
 
@@ -77,7 +78,6 @@ class MainApp(MDApp):
             self.root.ids.details_item.secondary_text = '[b]Files Sent: [/b]0'
             self.root.ids.details_item.tertiary_text = '[b]Files Received: [/b]0'
 
-
     def get_ip(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
@@ -87,41 +87,32 @@ class MainApp(MDApp):
         except Exception:
             IP = '127.0.0.1'
         finally:
-            s.close()        
+            s.close()
         return IP
 
         # hostname = socket.getfqdn()
         # myipaddress = socket.gethostbyname(hostname)
 
-        
-        
-
     def build(self):
         self.theme_cls.primary_palette = "DeepPurple"
         self.theme_cls.accent_palette = 'Green'
 
-
-
     def create_server(self):
         self.root.ids.bottomnav.switch_tab('filesharing')
-        self.new_server = server.Server(self.root.ids.progress, self.root.ids.reception, self.root.ids.details_item, self.root.ids.sentreceived, self.root.ids.received)
+        self.new_server = server.Server(self.root.ids.progress, self.root.ids.reception,
+                                        self.root.ids.details_item, self.root.ids.sentreceived, self.root.ids.received)
         try:
             self.server_thread = Thread(target=self.new_server.start_server)
-            self.server_thread.daemon =True
+            self.server_thread.daemon = True
             self.server_thread.start()
             toast("Server started")
         except Exception as e:
             toast(e)
-        
 
-    
     # def on_quit(self):
         # with shelve.open('./save_files/mydata') as shelf_file:
         #     shelf_file['files_sent'] = self.root.ids.details_item.secondary_text.text
         #     shelf_file['files_received'] = self.root.ids.details_item.tertiary_text.text
-
-
-
 
         # try:
         #     self.new_server.skt.close()
@@ -133,15 +124,7 @@ class MainApp(MDApp):
         #         print(e)
         #         pass
 
-
-
-
-
-    
-
     #------------------Dialog box-----------------#
-
-
 
     def show_recipient_dialog(self):
         if not self.dialog:
@@ -155,13 +138,10 @@ class MainApp(MDApp):
     def close_dialog(self):
         self.dialog.dismiss()
 
-
     #------------------End Dialog--------------------#
-    
 
     def switchtab(self):
         self.root.ids.bottomnav.switch_tab('filesharing')
-
 
     def toast_ip(self):
         toast("Copied")
@@ -169,12 +149,11 @@ class MainApp(MDApp):
     def receiver_ipaddress(self, text):
         self.receiveraddress = text
 
-
-
-
     #----------------File Manager---------------------------#
+
     def file_manager_open(self):
-        self.file_manager.show(primary_ext_storage)  # output manager to the screen
+        # output manager to the screen
+        self.file_manager.show(primary_ext_storage)
         self.manager_open = True
 
     def select_path(self, path):
@@ -189,7 +168,8 @@ class MainApp(MDApp):
         try:
             self.switch_tab()
             self.root.ids.reception.text = f"Sending: {os.path.basename(path)}"
-            self.new_client = client.Client(self.receiveraddress, 5001, path, self.root.ids.progress, self.root.ids.details_item)
+            self.new_client = client.Client(
+                self.receiveraddress, 5001, path, self.root.ids.progress, self.root.ids.details_item)
             self.client_thread = Thread(target=self.new_client.connect)
             self.client_thread.daemon = True
             self.client_thread.start()
@@ -197,7 +177,6 @@ class MainApp(MDApp):
             toast(e)
             with open('myerrorlog.txt', 'w') as f:
                 f.write(e)
-            
 
     def exit_manager(self, *args):
         '''Called when the user reaches the root of the directory tree.'''
@@ -213,7 +192,6 @@ class MainApp(MDApp):
                 self.file_manager.back()
         return True
 
-    
     #------------------End file manager------------------------#
 
 
